e6276019bba5393f7a02d5d2fe8aeaf52e663e14
juise <juise@juise-lab-pc>
2013-10-29 08:50:43 +0200

    uploadPhoto

--- spring-social-showcase/src/main/java/org/springframework/social/showcase/vkontakte/VKontaktePhotosController.java
+++ spring-social-showcase/src/main/java/org/springframework/social/showcase/vkontakte/VKontaktePhotosController.java
@@ -15,15 +15,17 @@
  */
 package org.springframework.social.showcase.vkontakte;
 
+import javax.inject.Inject;
+
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.vkontakte.api.PhotosOperations;
+import org.springframework.social.vkontakte.api.UploadServer;
 import org.springframework.social.vkontakte.api.VKontakte;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.inject.Inject;
 
 @Controller
 public class VKontaktePhotosController {
@@ -37,7 +39,14 @@ public class VKontaktePhotosController {
 		if (connection == null) {
 			return "redirect:/connect/vkontakte";
 		}
-		model.addAttribute("uploadserver", connection.getApi().photosOperations().getUploadServer());
+		
+		PhotosOperations photosOperations = connection.getApi().photosOperations();
+		UploadServer uploadServer = photosOperations.getUploadServer();
+		
+		model.addAttribute("uploadserver", uploadServer);
+		
+		photosOperations.uploadPhoto(uploadServer.getUpload_url(), "photoFileLocation");
+		
 		return "vkontakte/photos";
 	}
 
