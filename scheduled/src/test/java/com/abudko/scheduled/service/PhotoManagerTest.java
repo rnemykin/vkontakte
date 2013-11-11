2fe94b9153fa918b0ec112655432e99e416569d5
alexei <alexei.budko@netum.fi>
2013-11-11 08:37:31 +0200

    fixed photo data

--- scheduled/src/test/java/com/abudko/scheduled/service/PhotoManagerTest.java
+++ scheduled/src/test/java/com/abudko/scheduled/service/PhotoManagerTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/spring/test-app-config.xml" })
-public class PhotoManagerTest {
+public abstract class PhotoManagerTest {
 
     @Autowired
     private PhotoManager photoManager;
