64a2bfd99ee72a2ce355a4d80fb202c869d5b9ce
alexei <alexei.budko@netum.fi>
2013-11-12 13:13:55 +0200

    daily and minute intervals

---
+++ scheduled/src/main/java/com/abudko/scheduled/jobs/csv/DayIntervalScheduler.java
@@ -0,0 +1,36 @@
+package com.abudko.scheduled.jobs.csv;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.abudko.scheduled.jobs.Scheduler;
+import com.abudko.scheduled.service.PhotoManager;
+
+public class DayIntervalScheduler implements Scheduler {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+    
+    @Autowired
+    private PhotoManager photoManager;
+    
+    @Value("#{scheduledProperties['dayIntervalCsvFile']}")
+    private String csvResourcePath;
+    
+    @Value("#{scheduledProperties['dayIntervalDumpFile']}")
+    private String dumpFileLocation;
+
+    public void schedule() {
+        log.info("********* Start scheduled scanning *******");
+        try {
+
+            photoManager.publish(csvResourcePath, dumpFileLocation);
+            
+            log.info("********* End scheduled scanning *******");
+
+        } catch (Exception e) {
+            log.error("Exception happened during scheduled scan: ", e);
+        }
+    }
+}
