3c61029ea172dadebe8adef6d05623588bd99275
juise <juise@juise-lab-pc>
2013-11-13 13:24:06 +0200

    minor fix

--- scheduled/src/main/java/com/abudko/scheduled/jobs/huuto/CleanScheduler.java
+++ scheduled/src/main/java/com/abudko/scheduled/jobs/huuto/CleanScheduler.java
@@ -2,11 +2,8 @@ package com.abudko.scheduled.jobs.huuto;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 import com.abudko.scheduled.jobs.Scheduler;
-import com.abudko.scheduled.service.PhotoManager;
 
 public class CleanScheduler implements Scheduler {
 
