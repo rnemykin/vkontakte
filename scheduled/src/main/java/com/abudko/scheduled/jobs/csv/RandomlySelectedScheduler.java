17d37dd477d6de62d237206e2ad195816a7fa83e
juise <juise@juise-lab-pc>
2013-11-19 07:23:44 +0200

    randomly selected

--- scheduled/src/main/java/com/abudko/scheduled/jobs/csv/RandomlySelectedScheduler.java
+++ scheduled/src/main/java/com/abudko/scheduled/jobs/csv/RandomlySelectedScheduler.java
@@ -14,7 +14,7 @@ public class RandomlySelectedScheduler implements Scheduler {
     private Logger log = LoggerFactory.getLogger(getClass());
     
     @Autowired
-    @Qualifier("groupPhotoManager")
+    @Qualifier("randomSelectedGroupPhotoManager")
     private PhotoManager photoManager;
     
     @Value("#{scheduledProperties['randomlySelectedCsvFile']}")
