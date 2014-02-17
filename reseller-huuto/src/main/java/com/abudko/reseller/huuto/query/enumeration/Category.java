89de52316917bb23b36614e4479a79075386f44f
juise <juise@juise-lab-pc>
2014-02-17 10:57:53 +0200

    new data

--- reseller-huuto/src/main/java/com/abudko/reseller/huuto/query/enumeration/Category.java
+++ reseller-huuto/src/main/java/com/abudko/reseller/huuto/query/enumeration/Category.java
@@ -4,21 +4,21 @@ package com.abudko.reseller.huuto.query.enumeration;
 
 public enum Category {
 
-    TALVIHAALARI("talvihaalari%20OR%20toppahaalari", "Зим. комбинезоны", null),
+    TALVIHAALARI("talvihaalari%20OR%20toppahaalari%20OR%20talvipuku%20OR%20toppapuku", "Зим. комбинезоны", null),
     
     TALVIHOUSUT("talvihousut%20OR%20toppahousut", "Зим. штаны", null),
     
     TALVITAKKI("talvitakki%20OR%20toppatakki", "Зим. куртки", null),
 
-    VALIKAUSIHAALARI("vÃ¤likausihaalari", "Деми комбинезоны", null),
+    VALIKAUSIHAALARI("vÃ¤likausihaalari%20OR%20vÃ¤likausipuku", "Деми комбинезоны", null),
 
     VALIKAUSIHOUSUT("vÃ¤likausihousut", "Деми штаны", null),
     
     VALIKAUSITAKKI("vÃ¤likausitakki", "Деми куртки", null),
     
-    VILLAHAALARI("villahaalari%20OR%20fleecehaalari", "Поддевы", null),
+    VILLAHAALARI("villahaalari%20OR%20fleecehaalari%20OR%20villapuku%20OR%20fleecepuku", "Поддевы", null),
     
-    SADEHAALARI("sadehaalari%20OR%20kurahaalari%20OR%20sadehousut%20OR%20kurahousut%20OR%20sadetakki%20OR%20kuratakki", "Прорезиненная одежда", null),
+    SADEHAALARI("sadehaalari%20OR%20kurahaalari%20OR%20sadehousut%20OR%20kurahousut%20OR%20sadetakki%20OR%20kuratakki%20OR%20kurapuku%20OR%20sadepuku", "Прорезиненная одежда", null),
 
     TALVIKENGAT("talvikengÃ¤t", "Зим. обувь", new String[] {"728","736"}),
     
