2c46ce95fc07f7cc8a94b6c009d20b347fbd94fa
alexei <alexei.budko@netum.fi>
2014-03-25 12:34:13 +0200

    test abstract

--- scheduled/src/test/java/com/abudko/scheduled/jobs/huuto/PublishSchedulerIntegrationTest.java
+++ scheduled/src/test/java/com/abudko/scheduled/jobs/huuto/PublishSchedulerIntegrationTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/spring/test-app-config.xml" })
-public class PublishSchedulerIntegrationTest {
+public abstract class PublishSchedulerIntegrationTest {
 
     @Autowired
     private PublishScheduler publishScheduler;
