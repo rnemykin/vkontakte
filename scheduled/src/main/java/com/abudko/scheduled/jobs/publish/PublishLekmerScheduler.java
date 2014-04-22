c9af5af4e1166417fd4be560e795e6bb75dbc233
alexei <alexei.budko@netum.fi>
2014-04-22 12:29:37 +0300

    new data

--- scheduled/src/main/java/com/abudko/scheduled/jobs/publish/PublishLekmerScheduler.java
+++ scheduled/src/main/java/com/abudko/scheduled/jobs/publish/PublishLekmerScheduler.java
@@ -53,7 +53,7 @@ public class PublishLekmerScheduler implements Scheduler {
         publishLekmerInternal("toppapuku", "TALVIHAALARI", 0);
         publishLekmerInternal("kaksiosainen+setti+talvi", "TALVIHAALARI", 0);
         publishLekmerInternal("wind+haalari", "VALIKAUSIHAALARI", 0);
-        publishLekmerInternal("sadeasut", "SADEHAALARI", 30);
+        publishLekmerInternal("sadeasut", "SADEHAALARI", 35);
     }
 
     private void publishLekmerInternal(String query, String categoryenum, int limit)
