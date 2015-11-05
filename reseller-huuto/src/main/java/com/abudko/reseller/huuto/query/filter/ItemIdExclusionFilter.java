333630fca6fd1e37e547858f00da78c0b966ccdd
alexei <alexei.budko@netum.fi>
2015-11-05 10:45:59 +0200

    exclusion

--- reseller-huuto/src/main/java/com/abudko/reseller/huuto/query/filter/ItemIdExclusionFilter.java
+++ reseller-huuto/src/main/java/com/abudko/reseller/huuto/query/filter/ItemIdExclusionFilter.java
@@ -11,23 +11,24 @@ import com.abudko.reseller.huuto.query.service.list.ListResponse;
 @Component
 public class ItemIdExclusionFilter extends AbstractNotEmptyFilter {
 
-    private static final List<String> EXCLUSION_LIST = Arrays.asList("LE", "LEJ81899", "LE475521 056",
-            "LE13092862 BRIGHT", "LE13092852 BRIGHT", "LE13122 DARK PURP", "LE13112 DARK PURP", "LE100078");
+	private static final List<String> EXCLUSION_LIST = Arrays.asList("LE", "LEJ81899", "LE475521 056",
+			"LE13092862 BRIGHT", "LE13092852 BRIGHT", "LE13122 DARK PURP", "LE13112 DARK PURP", "LE100078",
+			"LE5-74200-203-21", "LE5-74200-1648-21", "LE5-74200-1709-21", "");
 
-    @Override
-    protected String getValue(ListResponse queryListResponse) {
-        ItemResponse itemResponse = queryListResponse.getItemResponse();
-        if (itemResponse == null) {
-            return " ";
-        }
-        if (shouldInclude(itemResponse)) {
-            return itemResponse.getId();
-        }
+	@Override
+	protected String getValue(ListResponse queryListResponse) {
+		ItemResponse itemResponse = queryListResponse.getItemResponse();
+		if (itemResponse == null) {
+			return " ";
+		}
+		if (shouldInclude(itemResponse)) {
+			return itemResponse.getId();
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    private boolean shouldInclude(ItemResponse itemResponse) {
-        return !EXCLUSION_LIST.contains(itemResponse.getId());
-    }
+	private boolean shouldInclude(ItemResponse itemResponse) {
+		return !EXCLUSION_LIST.contains(itemResponse.getId());
+	}
 }
