cf3c11d67de247477d7133a0b21a9d71df850ace
alexei <alexei.budko@netum.fi>
2015-12-10 12:53:19 +0200

    typo

--- reseller-huuto/src/main/java/com/abudko/reseller/huuto/query/rules/html/HtmlSearchQueryRules.java
+++ reseller-huuto/src/main/java/com/abudko/reseller/huuto/query/rules/html/HtmlSearchQueryRules.java
@@ -10,7 +10,7 @@ public class HtmlSearchQueryRules implements SearchQueryRules {
 
     @Override
     public void apply(SearchParams searchParams) {
-        searchParams.setArea("Helsinki%20OR%20Espoo%20OR%20Vantaa%20OR%20KauniainenOR%20KeravaOR%20Tuusula");
+        searchParams.setArea("Helsinki%20OR%20Espoo%20OR%20Vantaa%20OR%20KauniainenOR%20Kerava%20OR%20Tuusula");
         searchParams.setClassification("new");
 //        searchParams.setSellstyle("buy-now");
     }
